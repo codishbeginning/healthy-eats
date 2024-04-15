17978b363087d0d6385cae2797dd9450c5d35a56
Sarthak Kansal <stk.kansal@gmail.com>
2024-04-14 18:13:41 -0700

    Add NutritionalInformationFetcher class

---
+++ src/nutritional_information_fetcher.py
@@ -0,0 +1,11 @@
+""" This class defines methods that fetch nutritional information"""
+
+class NutritionalInformationFetcher:
+    """ This class defines methods that fetch nutritional information"""
+
+    def __init__(self):
+        pass
+
+    def fetch_nutritional_information(self):
+        """ This method fetches nutritional information"""
+        return None
