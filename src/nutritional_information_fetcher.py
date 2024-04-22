165f477fc2b055fe6ea6a355b00f8b978260bd0d
Rishi Khante <rishikhante@gmail.com>
2024-04-22 01:16:28 -0400

    defined method to get alternate recipes

--- src/nutritional_information_fetcher.py
+++ src/nutritional_information_fetcher.py
@@ -13,3 +13,7 @@ class NutritionalInformationFetcher:
     def is_suitable_for_diabetics(self, ingredients_list: list):
         """ This method helps determine if the food is suitable for a diabetic patient""" 
         return True
+
+    def get_alernate_recipes(self, ingredients_list: list) -> list:
+        """ This method takes a list of ingredients and suggests recipes to the user""" 
+        return []
